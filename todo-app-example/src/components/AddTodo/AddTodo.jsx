b77b0346d7fd329ec60b7f4e2dea1cb73ea0096c
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-01-25 16:44:12 +0100

    Added code

--- todo-app-example/src/components/AddTodo/AddTodo.jsx
+++ todo-app-example/src/components/AddTodo/AddTodo.jsx
@@ -1,12 +1,16 @@
+import './AddTodo.css';
+
 function AddTodo(props) {
     const { newTodo } = props;
     let todo = '';
 
     return (
-        <section>
+        <section className='add-todo'>
             <input type="text" placeholder="Ange todo"
+            className='add-todo__input'
             onChange={ (event) => { todo = event.target.value; console.log(todo) }} />
-            <button onClick={ () => { newTodo(todo) }}>Lägg till</button>
+            <button className='add-todo__button'
+            onClick={ () => { newTodo(todo) }}>Lägg till</button>
         </section>
     )
 }
